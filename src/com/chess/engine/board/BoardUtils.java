28cd0b9aa9442a728e12288dd379874294a2c57a
Mikhail Permyakov <mikhailpermyakov@yahoo.com>
2017-06-26 15:49:05 +0700

    in the middle of vid6. test

--- src/com/chess/engine/board/BoardUtils.java
+++ src/com/chess/engine/board/BoardUtils.java
@@ -1,7 +1,16 @@
 package com.chess.engine.board;
 
 public class BoardUtils {
-    public static final boolean[] FIRST_COLUMN = null;
+    public static final boolean[] FIRST_COLUMN = initColumn(0);
+
+    private static boolean[] initColumn(int columnNumber) {
+        final boolean[] column = new boolean[64];
+        do{
+            column[columnNumber] = true;
+            columnNumber += 8;
+        } while (columnNumber < 64);
+        return column;
+    }
     public static final boolean[] SECOND_COLUMN = null;
     public static final boolean[] SEVENTH_COLUMN = null;
     public static final boolean[] EIGHTH_COLUMN = null;
--- src/com/chess/engine/board/Move.java
+++ src/com/chess/engine/board/Move.java
@@ -1,7 +1,5 @@
 package com.chess.engine.board;
 
-/**
- * Created by Михаил on 25.06.2017.
- */
+
 public class Move {
 }
