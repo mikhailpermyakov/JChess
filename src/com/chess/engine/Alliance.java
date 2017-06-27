ff159313f89d3e4c11729511c4439a394cf71f1a
Mikhail Permyakov <mikhailpermyakov@yahoo.com>
2017-06-28 00:59:21 +0700

    after implementing pawn basics (vid 10)

--- src/com/chess/engine/Alliance.java
+++ src/com/chess/engine/Alliance.java
@@ -4,6 +4,18 @@ package com.chess.engine;
  * Created by Михаил on 25.06.2017.
  */
 public enum Alliance {
-    WHITE,
-    BLACK
+    WHITE {
+        @Override
+        public int getDirection() {
+            return -1;
+        }
+    },
+    BLACK {
+        @Override
+        public int getDirection() {
+            return 1;
+        }
+    };
+
+    public abstract int getDirection();
 }
