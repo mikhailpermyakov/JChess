6b77a91a6cdc77b05f9fe142488c3d9264dc3fc4
Mikhail Permyakov <mikhailpermyakov@yahoo.com>
2017-06-30 18:44:15 +0700

    after implementing the board part 3 (vid 16)

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -43,6 +43,10 @@ public abstract class Tile {
         }
 
         @Override
+        public String toString(){
+            return "-";
+        }
+        @Override
         public boolean isTileOccupied(){
             return false;
         }
@@ -57,6 +61,11 @@ public abstract class Tile {
         private OccupiedTile(final int tileCoordinate, Piece pieceOnTile){
             super(tileCoordinate);
             this.pieceOnTile = pieceOnTile;
+        }
+        @Override
+        public String toString(){
+            return getPiece().getPieceAlliance().isBlack() ? getPiece().toString().toLowerCase() :
+                   getPiece().toString();
         }
 
         @Override
