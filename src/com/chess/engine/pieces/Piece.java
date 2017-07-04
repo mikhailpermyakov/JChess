712a43b027c9e08e734c1d2156ec6e8518f9786e
Mikhail Permyakov <mikhailpermyakov@yahoo.com>
2017-07-04 11:45:50 +0700

    after implementing the move part 3 (vid 23)

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -12,6 +12,7 @@ public abstract class Piece {
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
     protected final boolean isFirstMove;
+    private final int cachedHashCode;
 
     Piece(final PieceType pieceType,
           final int piecePosition,
@@ -21,8 +22,33 @@ public abstract class Piece {
         this.piecePosition = piecePosition;
         //TODO more work here!!!
         this.isFirstMove = false;
+        this.cachedHashCode = computeHashCode();
     }
 
+    protected int computeHashCode(){
+        int result = pieceType.hashCode();
+        result = 31 * result + pieceAlliance.hashCode();
+        result = 31 * result + piecePosition;
+        result = 31 * result + (isFirstMove ? 1 : 0);
+        return result;
+    };
+
+    @Override
+    public boolean equals(final Object other){
+        if(this == other){
+            return true;
+        }
+        if(!(other instanceof Piece)){
+            return false;
+        }
+        final Piece otherPiece = (Piece) other;
+        return piecePosition == otherPiece.getPiecePosition() && pieceType == otherPiece.getPieceType() &&
+               pieceAlliance == otherPiece.getPieceAlliance() && isFirstMove == otherPiece.isFirstMove();
+    }
+    @Override
+    public int hashCode(){
+        return this.cachedHashCode;
+    }
     public int getPiecePosition(){
         return this.piecePosition;
     }
