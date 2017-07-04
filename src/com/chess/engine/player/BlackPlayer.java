044855b92db484685f9c759349198c12c052b3bc
Mikhail Permyakov <mikhailpermyakov@yahoo.com>
2017-07-05 00:34:10 +0700

    after implementing the move part 7 (vid 27)

--- src/com/chess/engine/player/BlackPlayer.java
+++ src/com/chess/engine/player/BlackPlayer.java
@@ -3,9 +3,13 @@ package com.chess.engine.player;
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by Михаил on 01.07.2017.
@@ -31,4 +35,34 @@ public class BlackPlayer extends Player {
     public Player getOpponent() {
         return this.board.whitePlayer();
     }
+
+    @Override
+    protected Collection<Move> calculateKingCastles(Collection<Move> playerLegals, Collection<Move> opponentsLegals) {
+        final List<Move> kingCastles = new ArrayList<>();
+
+        if(this.playerKing.isFirstMove() && !this.isInCheck()){
+            //blacks king side castle
+            if(!this.board.getTile(5).isTileOccupied() &&
+               !this.board.getTile(6).isTileOccupied()){
+                final Tile rookTile = this.board.getTile(7);
+                if(rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()){
+                    if(Player.calculateAttacksOnTile(5, opponentsLegals).isEmpty() &&
+                       Player.calculateAttacksOnTile(6, opponentsLegals).isEmpty() &&
+                            rookTile.getPiece().getPieceType().isRook()){
+                        //TODO add a castlemove!
+                        kingCastles.add(null);
+                    }
+                }
+            }
+            if(!this.board.getTile(1).isTileOccupied() &&
+               !this.board.getTile(2).isTileOccupied() &&
+               !this.board.getTile(3).isTileOccupied()){
+                final Tile rookTile = this.board.getTile(0);
+                if(rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()){
+                    //TODO add castle move
+                    kingCastles.add(null);
+                }
+            }
+        }
+        return ImmutableList.copyOf(kingCastles);    }
 }
